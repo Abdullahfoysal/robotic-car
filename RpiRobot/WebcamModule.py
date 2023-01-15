ac5321cae9dcfb5ac76e312ca8400db86c7beac3
Abdullahfoysal <abdullah32@student.sust.edu>
2023-01-15 17:53:36 +0600

    camera bug fix

--- RpiRobot/WebcamModule.py
+++ RpiRobot/WebcamModule.py
@@ -3,13 +3,13 @@ import cv2
 cap = cv2.VideoCapture(0)
  
 def getImg(display= True,size=[480,240]):
-    _, img = cap.read()
-    img = cv2.resize(img,(size[0],size[1]))
-    if display:
+    success, img = cap.read()
+    img = cv2.resize(img,(480,240))
+    if success:
         cv2.imshow('IMG',img)
-        print(img.shape)
     return img
  
 if __name__ == '__main__':
     while True:
         img = getImg(True)
+        cv2.waitKey(1)
