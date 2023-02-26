291d0b829a633bff6695414e40771a77eb2e073a
Abdullah Al Foysal <abdullahfoysal32@gmail.com>
2023-02-26 16:32:28 +0600

    socket in predestrain detect

--- predestraint/SockectMain.py
+++ predestraint/SockectMain.py
@@ -2,6 +2,52 @@ from tensorflow.keras.models import load_model
 import cv2
 import numpy as np
 from PIL import Image
+#socket start 
+import eventlet
+import socketio
+import base64
+from PIL import Image
+from io import BytesIO
+
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
+
+def processing():
+    data = []
+    img_path = '/Users/foysalmac/Desktop/robotic-car/predestraint/processImage.png'
+    curr_img = cv2.imread(img_path)
+    image_fromarray = Image.fromarray(curr_img, 'RGB')
+    resize_image = image_fromarray.resize((30, 30))
+    data.append(np.array(resize_image))
+    X_test = np.array(data)
+    X_test = X_test / 255
+    preds = np.argmax(model.predict(X_test))
+   # print(preds)
+    return preds
+
+def decodeImage(data):
+    im = Image.open(BytesIO(base64.b64decode(data)))
+    im.save('processImage.png', 'PNG')
+    return processing()
+
+    
+
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+
+@sio.event
+def messageFromClient(sid, data):
+    print('messageFromClient ', "data['img']")
+    value = decodeImage(data['img'])
+    print("prdicted: ",value)
+    sio.emit('messageFromServer',{'gottemp': sign[value]})
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+    
+#socket End
 
 
 sign = [  "Speed limit (20km/h)",  "Speed limit (30km/h)",  "Speed limit (50km/h)",  "Speed limit (60km/h)",  "Speed limit (70km/h)",  "Speed limit (80km/h)",  "End of speed limit (80km/h)",  "Speed limit (100km/h)",  "Speed limit (120km/h)",  "No passing",  "No passing for vechiles over 3.5 metric tons",  "Right-of-way at the next intersection",  "Priority road",  "Yield",  "Stop",  "No vechiles",  "Vechiles over 3.5 metric tons prohibited",  "No entry",  "General caution",  "Dangerous curve to the left",  "Dangerous curve to the right",  "Double curve",  "Bumpy road",  "Slippery road",  "Road narrows on the right",  "Road work",  "Traffic signals",  "Pedestrians",  "Children crossing",  "Bicycles crossing",  "Beware of ice/snow",  "Wild animals crossing",  "End of all speed and passing limits",  "Turn right ahead",  "Turn left ahead",  "Ahead only",  "Go straight or right",  "Go straight or left",  "Keep right",  "Keep left",  "Roundabout mandatory",  "End of no passing",  "End of no passing by vechiles over 3.5 metric tons"]
@@ -13,28 +59,15 @@ model = load_model('/Users/foysalmac/Desktop/robotic-car/predestraint/best_model
 
 
 
-def processing(img):
-    data = []
-    #img_path = '/home/pi/Desktop/robotic-car/predestraint/go_ahead.png'
-    #curr_img = cv2.imread(img_path)
-    image_fromarray = Image.fromarray(img, 'RGB')
-    resize_image = image_fromarray.resize((30, 30))
-    data.append(np.array(resize_image))
-    X_test = np.array(data)
-    X_test = X_test / 255
-    preds = np.argmax(model.predict(X_test))
-    return preds
+
 
 
 if __name__ == '__main__':
-    while True:
+    eventlet.wsgi.server(eventlet.listen(('', 5556)), app)
+  # while True:
         #img = wM.getImg(True, size=[240, 120])
         #img = getImg(True)
-        img_path = '/Users/foysalmac/Desktop/robotic-car/predestraint/go_ahead.png'
-        curr_img = cv2.imread(img_path)
-        value =  processing(curr_img)
-        print(value)
-        print(sign[value])
+      
       
         
-        cv2.waitKey(1)
+      #  cv2.waitKey(1)
---
+++ predestraint/client.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 14 16:01:44 2023
+
+@author: foysalmac
+"""
+import cv2
+import socketio
+import base64
+from PIL import Image
+from io import BytesIO
+
+cap = cv2.VideoCapture(0)
+
+def getImg(display= True,size=[480,240]):
+    _, img = cap.read()
+    img = cv2.resize(img,(size[0],size[1]))
+    if display:
+        cv2.imshow('IMG',img)
+    return img
+
+sio = socketio.Client()
+
+
+def encodeImageFromLocal():
+    with open("go_ahead.png", "rb") as image_file:
+        print("heelo")
+        data = base64.b64encode(image_file.read())
+    
+    return data
+
+def encodedImage(img):
+    data = base64.b64encode(img)
+    return data
+
+def send_image(img):
+    img = encodedImage(img)
+    sio.emit('messageFromClient',{'img': img})
+
+def send_sensor_reading():
+    while True:
+        img = getImg(True)
+        #send_image(img)
+        #sio.sleep(5)
+        cv2.waitKey(1)
+
+@sio.event
+def connect():
+    print('connection established')
+    sio.start_background_task(send_sensor_reading)
+
+@sio.event
+def messageFromServer(data):
+    print('messageFromServer : prediction: ', data)
+    
+   
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+sio.connect('http://localhost:5556')
+sio.wait()
+
